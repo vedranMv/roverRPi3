b622bd977e4261989fbfb3057c9095dee14febe4
Vedran <vedran.mikov@gmail.com>
2017-03-25 14:18:22 +0100

    Integrated into kernel, can periodically check socket health.

--- roverKernel/network/dataStream.cpp
+++ roverKernel/network/dataStream.cpp
@@ -10,14 +10,88 @@
 #if defined(__HAL_USE_ESP8266__)
 #include "roverKernel/libs/myLib.h"
 #include "roverKernel/esp8266/esp8266.h"
+#include "roverKernel/serialPort/uartHW.h"
 
-DataStream::DataStream(): _socketID(0), _port(0), _socket(0)
+
+#if defined(__USE_TASK_SCHEDULER__)
+/*
+ * Interface with task scheduler - provides memory space and function to call in
+ * order for task scheduler to request service from this module. Not defined
+ * within the class as it's enough to have only one of these to check all opened
+ * streams.
+ */
+static _kernelEntry _dsKer;
+
+/**
+ * Callback routine to invoke service offered by this module from task scheduler
+ * @note It is assumed that once this function is called task scheduler has
+ * already copied required variables into the memory space provided for it.
+ */
+void _DATAS_KernelCallback(void)
+{
+    //  Check for null-pointer
+    if (_dsKer.argN == 0)
+        return;
+    /*
+     *  Data in args[] contains bytes that constitute arguments for function
+     *  calls. The exact representation(i.e. whether bytes represent ints, floats)
+     *  of data is known only to individual blocks of switch() function. There
+     *  is no predefined data separator between arguments inside args[].
+     */
+    switch (_dsKer.serviceID)
+    {
+    /*
+     * Keep-alive event, check if the socket is still alive, if not try to reconnect
+     * args[] = pointerToDatastreamObject(DataStream*)
+     * retVal ESP library error code (if > 5); or socket ID (if <=5)
+     */
+    case DATAS_T_KA:
+        {
+            //  Pointer is encoded into integer number
+            uint32_t ptr = 0;
+            memcpy(&ptr, (void*)_dsKer.args, 4);
+            DataStream *ds = (DataStream*)ptr;
+
+            _espClient *socket = ESP8266::GetI().GetClientBySockID(ds->socketID);
+
+            /*
+             * If socket has been closed GetClientBySockID returns 0. To reopen
+             * it we just call BindToScoketID as it already handles that
+             */
+            if (socket == 0)
+                ds->BindToSocketID(ds->socketID);
+        }
+        break;
+    default:
+        break;
+    }
+
+}
+
+/**
+ * Registers data stream as a kernel module if compiled with task scheduler
+ */
+void DataStream_InitHW()
+{
+    //  Register module services with task scheduler
+    _dsKer.callBackFunc = _DATAS_KernelCallback;
+    TS_RegCallback(&_dsKer, DATAS_UID);
+}
+
+#endif
+
+
+
+///-----------------------------------------------------------------------------
+///                      Class constructor & destructor                 [PUBLIC]
+///-----------------------------------------------------------------------------
+DataStream::DataStream(): socketID(0), _port(0), _socket(0), _keepAlive(false)
 {
     memset((void*)_serverip, 0, sizeof(_serverip));
 }
 
 DataStream::DataStream(uint8_t *ip, uint16_t port)
-    : _socketID(0), _port(port), _socket(0)
+    : socketID(0), _port(port), _socket(0), _keepAlive(false)
 {
     uint8_t i;
 
@@ -26,29 +100,68 @@ DataStream::DataStream(uint8_t *ip, uint16_t port)
     memcpy((void*)_serverip, (void*)ip, i);
 }
 
-
-uint8_t DataStream::BindToScoketID(uint8_t sockID)
+DataStream::~DataStream()
 {
+    if (_keepAlive)
+    {
+        //TODO: Delete task from task scheduler
+    }
+    //  Close the socket before deleting data stream
+    _socket->Close();
+}
+
+///-----------------------------------------------------------------------------
+///                      Public member functions                        [PUBLIC]
+///-----------------------------------------------------------------------------
+
+/**
+ * Bind data stream to a specific socket id. If socket already exists it just
+ * binds it, if it doesn't it attempts to open it with first free ID and bind
+ * data stream to that ID. Socket id bound to this stream is saved internally
+ * and returned from this function.
+ * @param sockID socket ID as returned from ESP chip (0 - 4) of a socket to bind to
+ * @return socket ID to which data stream was eventually bound; 127 on error
+ */
+uint8_t DataStream::BindToSocketID(uint8_t sockID)
+{
+
+#if defined(__USE_TASK_SCHEDULER__)
+    if (!_keepAlive)
+    {
+    //  Schedule periodic check for health of the underlying socket, period 4s
+    TaskScheduler::GetI().SyncTask(DATAS_UID, DATAS_T_KA, -4000, true, T_PERIODIC);
+    TaskScheduler::GetI().AddArg<uint32_t>((uint32_t)this);
+    _keepAlive = true;
+    }
+#endif
+
     //  Check if the socket is already opened
     _socket = ESP8266::GetI().GetClientBySockID(sockID);
 
     if (_socket!= 0)
-        _socketID = sockID;
+        socketID = sockID;
     else
     {   // Attempt to open a socket if IP address exists
         if (_serverip[0] == 0)
             return 222;
-        _socketID = ESP8266::GetI().OpenTCPSock((char*)_serverip, _port, 1, sockID);
+        //  Attempt to open the socket and check for error codes (> max clients)
+        uint32_t status;
+        status = ESP8266::GetI().OpenTCPSock((char*)_serverip, _port, 1, sockID);
+        if (status < ESP_MAX_CLI)
+            socketID = status;
+        else
+            return 127;
         //  Get reference to opened socket
         _socket = ESP8266::GetI().GetClientBySockID(sockID);
     }
     //  As a confirmation return socket id
-    return _socketID;
+    return socketID;
 }
 
 /**
- * Send either a null terminated string with no buffer len, or any string
- * of a certain length
+ * Send either a null terminated string with no buffer len, or any string of a
+ * certain length through the stream. Function checks whether the bounded socket
+ * is still alive, if not tries to reopen it.
  * @note Wrapper for low-level espClient:: function
  * @param buffer
  * @param bufferLen
@@ -56,9 +169,12 @@ uint8_t DataStream::BindToScoketID(uint8_t sockID)
 void DataStream::Send(uint8_t *buffer, uint16_t bufferLen)
 {
     //  Check if the socket is still opened
-    _socket = ESP8266::GetI().GetClientBySockID(_socketID);
+    _socket = ESP8266::GetI().GetClientBySockID(socketID);
     if (_socket!= 0)
-        return;
+    {
+        //  If it isn't attempt to reopen it
+        BindToSocketID(socketID);
+    }
 
     _socket->SendTCP((char*)buffer, bufferLen);
 }
@@ -68,14 +184,15 @@ void DataStream::Send(uint8_t *buffer, uint16_t bufferLen)
  * @note Wrapper for low-level espClient:: function
  * @param buffer
  * @param bufferLen
- * @return
+ * @return true: if new data was put into buffer, false otherwise
  */
 bool DataStream::Receive(uint8_t *buffer, uint16_t *bufferLen)
 {
-    //  Check if the socket is still opened
-    _socket = ESP8266::GetI().GetClientBySockID(_socketID);
+    //  Check if the socket is still opened, if it isn't there's no use in
+    //  reopening it as there will be no new data to read; so just return
+    _socket = ESP8266::GetI().GetClientBySockID(socketID);
     if (_socket!= 0)
-        return;
+        return false;
 
     //  Check if there's response ready
     if (_socket->Ready())
--- roverKernel/network/dataStream.h
+++ roverKernel/network/dataStream.h
@@ -4,38 +4,83 @@
  *  Created on: Mar 16, 2017
  *      Author: Vedran Mikov
  *
- *  Data stream object that utilizes sockets handled by the ESP module
+ *  Data stream object that utilizes sockets handled by the ESP module. Maintains
+ *  a data stream with a server that allows exchanging data. Data can be any
+ *  uint8_t character and is not limited to readable range (0x00 - 0x7F). Library
+ *  can be integrated with task scheduler to periodically check if the stream is
+ *  opened and try to reconnect in case of a failure.
  *
- *  @version 1.0
- *  V1.0
+ *  @version 1.2
+ *  V1.0 - 17.3.2017
  *  +Created document
- *
+ *  +Functionality: Initialize data stream with server IP & port, bind to opened
+ *  socket or attempt to open new socket
+ *  V1.1 - 23.3.2017
+ *  +When sending, if socket is closed, attempt to reopen and rebind it to stream
+ *  V1.2 - 24.3.2017
+ *  +On first call to BindToSocketID() object registers periodic health-check of
+ *  connection to server. Attempts to reestablish lost connection.
  */
 #include "roverKernel/hwconfig.h"
 
 //  Makes sense to compile only if ESP module is being used
 #if !defined(ROVERKERNEL_NETWORK_DATASTREAM_H_) && defined(__HAL_USE_ESP8266__)
 #define ROVERKERNEL_NETWORK_DATASTREAM_H_
+
 #include "roverKernel/esp8266/espClient.h"
 
+//  Enable integration of this library with task scheduler but only if task
+//  scheduler is being compiled into this project
+#if defined(__HAL_USE_TASKSCH__)
+#define __USE_TASK_SCHEDULER__
+#endif  /* __HAL_USE_TASKSCH__ */
 
+//  Check if this library is set to use task scheduler
+#if defined(__USE_TASK_SCHEDULER__)
+    #include "roverKernel/taskScheduler/taskScheduler.h"
+    //  Unique identifier of this module as registered in task scheduler
+    #define DATAS_UID       4
+    //  Definitions of ServiceID for service offered by this module
+    #define DATAS_T_KA      0   //  Keep alive socket
+
+//  Function to register data stream as a kernel module into the task scheduler
+void DataStream_InitHW();
+
+#endif
+
+/**
+ * Definition of DataStream class. High level network communication object that
+ * utilizes network sockets handled by ESP8266 library to establish a two-way
+ * data stream with TCP server. Once the data stream has been bounded to a
+ * socket it maintains the connectivity through periodic health-checks (using
+ * task scheduler) and attempts reconnect in case of lost connection.
+ */
 class DataStream
 {
+    friend void _DATAS_KernelCallback(void);
     public:
         DataStream();
         DataStream(uint8_t *ip, uint16_t port);
+        ~DataStream();
 
-        uint8_t BindToScoketID(uint8_t sockID);
+        uint8_t BindToSocketID(uint8_t sockID);
 
         void    Send(uint8_t *buffer, uint16_t bufferLen = 0);
         bool    Receive(uint8_t *buffer, uint16_t *bufferLen);
 
-    private:
+        //  Socket ID as returned from ESP8266
+        uint8_t     socketID;
 
-        uint8_t     _socketID;
+    private:
+        //  String containing server IP address of underlying socket
         uint8_t     _serverip[20];
+        //  Port number of server to which this stream is opened
         uint16_t    _port;
+        //  Socket handle
         _espClient* _socket;
+        //  Turns true once this data stream has scheduled periodic checking
+        //  of socket's health (whether we're still connected to the server)
+        bool        _keepAlive;
 };
 
 
